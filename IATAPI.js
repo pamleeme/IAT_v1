e0b405ca3ed61b105972c6d0e0bda1b29cf63229
pamleeme <119915195+pamleeme@users.noreply.github.com>
2022-12-06 20:26:37 +0800

    Create IATAPI.js

---
+++ IATAPI.js
@@ -0,0 +1,50 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+		category1 : {
+			name : 'Male', //Will appear in the data.
+			title : {
+				media : {word : 'Male'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {word : 'Man'}, 
+    			{word : 'Boy'}, 
+    			{word : 'Manlike'}, 
+    			{word : 'Masculine'}, 
+    			{word : 'Paternal'}, 
+          {word : 'Brother'}, 
+          {word : 'Sir'}, 
+    			{word : 'Mister'}
+			], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 :	{
+			name : 'Female', //Will appear in the data.
+			title : {
+				media : {word : 'Female'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    {word : 'Woman'}, 
+    			{word : 'Girl'}, 
+    			{word : 'Feminine'}, 
+    			{word : 'Maternal'}, 
+    			{word : 'Miss'}, 
+          {word : 'Lady'},
+          {word : 'Demure'},
+    			{word : 'Mdm'}			
+      ], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/'
+		} 
+	});
+});
